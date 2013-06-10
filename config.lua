33b1fab0183acf404fca9e20b5fdf006b8c570ac
Kyle Manna <kyle@kylemanna.com>
2013-06-09 23:09:47 -0700

    release: First public release
    * Add real files to public git repo.
    * Future development will occur here.

---
+++ config.lua
@@ -0,0 +1,197 @@
+--
+-- ~/.imapfilter/config.lua
+--
+-- For more info see http://blog.kylemanna.com/linux/2013/06/09/use-imapfilter-to-filter-spam-part2/
+--
+require("os")
+require("io")
+require("posix")
+
+-- Update path for imapfilter relative files
+package.path = package.path .. ';' .. os.getenv("HOME") .. '/.imapfilter/?.lua'
+require("lua-popen3/pipe")
+
+--
+-- Trim whitespace from strings
+--
+function trim(s)
+	return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+
+--
+-- Filter the results through spamassassin
+--
+function filter(results)
+	local mailbox
+	local uid = {}
+	local orig = {}
+	local subresult_cnt = 32 -- Number of messages to process in each pass
+	local max_procs = 10 -- Number of spamassasin processes to run in parallel
+
+	for subresults, msgs in iter_msgs(results, subresult_cnt) do
+
+		-- Filter it through spamassassin
+		local status, allmsg = pipe_multi(msgs, max_procs, 'spamc')
+
+		for _, msg in pairs(allmsg) do
+			-- TODO Why doesn't the following work?
+			--pattern = "^X-Spam-Flag:\\s*YES"
+			local pattern = 'X-Spam-Flag:\\s*YES'
+			local match = regex_search(pattern, msg)
+
+			local result = 'SPAM'
+			if match == true then
+				account1.Spam:append_message(msg)
+			else
+				result = 'normal'
+				account1.INBOX:append_message(msg)
+			end
+
+			--pattern = 'Subject:\\s*(.*)\n'
+			--match2, cap = regex_search(pattern, msg)
+			if subject == nil then
+				subject = '(unknown)'
+			end
+			local out = '>> Msg "' .. subject ..  '" is ' .. result .. '\n'
+			io.write(out)
+		end
+
+		-- Make old messages as seen and keep them
+		-- Later we might delete them after we trust this filter
+		subresults:mark_seen()
+	end
+end
+
+--
+-- Split up a large imapfilter result into chunk sizes
+-- by iterating over it. Provides a way to elegantly
+-- handle large results without blowing up memory
+--
+function iter_msgs(results, chunk)
+
+	local i = 1
+	local last = 0
+	local max = #results
+
+	return function()
+		local subresults = {}
+		local msgs = {}
+		while i <= (last + chunk) and i <= max do
+			local mailbox, uid = table.unpack(results[i])
+			local mbox = mailbox[uid]
+			local msg = mbox:fetch_message()
+			table.insert(msgs, msg)
+			table.insert(subresults, results[i])
+			i = i + 1
+		end
+		last = i
+		if next(subresults) then
+			return Set(subresults), msgs
+		end
+	end
+end
+
+--
+-- Feed spam messages to sa-learn to teach the bayesian classifier
+--
+function sa_learn(learn_type, results, dest)
+	local learn_arg = '--' .. learn_type
+	local subresult_cnt = 32 -- Number of messages to process in each pass
+	local max_procs = 1 -- Number of sa-learn processes to run in parallel
+
+	for subresults, msgs in iter_msgs(results, subresult_cnt) do
+		local status = pipe_multi(msgs, max_procs, 'sa-learn', learn_arg)
+
+		--[[
+		for s in ipairs(status) do
+			io.write('>> sa-learn returned '..s..'\n')
+		end
+		--]]
+		subresults:move_messages(dest)
+	end
+end
+
+--
+-- Run in an infinite loop
+--
+function forever()
+
+	max_filter_size = 1024 * 256 -- 256 KB
+
+	account1:create_mailbox('Spam')
+	account1:create_mailbox('Spam/False Positives')
+	account1:create_mailbox('Spam/False Negatives')
+	account1:create_mailbox('Spam/False Positives/Processed')
+	account1:create_mailbox('Spam/False Negatives/Processed')
+
+	local unfiltered = account1['Unfiltered']
+	local spam = account1['Spam']
+	local false_pos = account1['Spam/False Positives']
+	local false_pos_done = account1['Spam/False Positives/Processed']
+	local false_neg = account1['Spam/False Negatives']
+	local false_neg_done = account1['Spam/False Negatives/Processed']
+
+	while true do
+
+		-- Loop over the results in the event a new message shows up
+		-- while we are proccessing earlier ones.
+		local unseen = unfiltered:is_unseen()
+
+		-- Just move the large messages
+		local large = unfiltered:is_larger(max_filter_size)
+		local results = unseen * large
+		results:copy_messages(account1.INBOX)
+		results:mark_seen()
+
+		-- Filter the remaining messages
+		local results = unseen - large
+		filter(results)
+
+
+		--
+		-- House keeping... Other work to do?
+		--
+
+		-- Check for messages older then x days in the original inbox
+		-- and delete them
+		results = unfiltered:is_older(14):delete_messages()
+		results = spam:is_older(60):delete_messages()
+
+		-- Teach spamassassin about the good email that was marked as spam
+		results = false_pos:is_smaller(max_filter_size)
+		sa_learn('ham', results, false_pos_done)
+
+		-- Teach spamassassin about the messages it missed
+		results = false_neg:is_smaller(max_filter_size)
+		sa_learn('spam', results, false_neg_done)
+
+		-- Block until something happens, assuming server supports IMAP IDLE
+		-- Note: there is still a window between checking unseen messages
+		-- and entering idle where we could miss a new arrival.  In that
+		-- case it will have to wait until another email arrives.
+		if #unfiltered:is_unseen() == 0 then
+			local update = unfiltered:enter_idle()
+		end
+	end
+end
+
+
+---------------
+--  Options  --
+---------------
+
+options.timeout = 120
+--options.subscribe = true
+
+
+----------------
+--  Accounts  --
+----------------
+-- include ~/.imapfilter/accounts.lua (assuming package.path is set correctly)
+require("accounts")
+
+
+forever()
+-- Daemon mode fails to properly recover when the remote server trips
+-- and the connection closes, so it needs to be run in a wrapper script.
+--become_daemon(600, forever)
---
+++ wrapper.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+import os
+import sys
+import gtk # ensure that the application name is correctly set
+import gnomekeyring as gkey
+import getpass
+import subprocess
+import time
+import signal
+import string
+
+class Keyring(object):
+    #
+    # Original author: http://www.rittau.org/blog/20070726-01
+    # Original code: http://www.rittau.org/gnome/python/keyring.py
+    #
+
+    def __init__(self, name, server, protocol):
+
+        self._name = name
+        self._server = server
+        self._protocol = protocol
+        self._keyring = gkey.get_default_keyring_sync()
+
+    def has_credentials(self):
+        try:
+            attrs = {"server": self._server, "protocol": self._protocol}
+            items = gkey.find_items_sync(gkey.ITEM_NETWORK_PASSWORD, attrs)
+            return len(items) > 0
+        except gkey.DeniedError:
+            return False
+
+    def get_credentials(self):
+        attrs = {"server": self._server, "protocol": self._protocol}
+        items = gkey.find_items_sync(gkey.ITEM_NETWORK_PASSWORD, attrs)
+        return (items[0].attributes["user"], items[0].secret)
+
+    def set_credentials(self, (user, pw)):
+        attrs = {
+                "user": user,
+                "server": self._server,
+                "protocol": self._protocol,
+            }
+        gkey.item_create_sync(gkey.get_default_keyring_sync(),
+                gkey.ITEM_NETWORK_PASSWORD, self._name, attrs, pw, True)
+
+#
+# Wrap the imapfilter binary since it doesn't handle bad server connections
+# well.  It needs to be restarted when it crashes.
+#
+class ImapFilterWrapper(object):
+
+    def __init__(self, **kwargs):
+        self.name = os.path.basename(sys.argv[0])
+        self.verbose = kwargs.get('verbose', False)
+        self.debug = kwargs.get('debug', False)
+        self.daemonize = kwargs.get('daemonize', True)
+
+        self.home = os.getenv('HOME') + '/.imapfilter'
+        self.pidfile = self.home + '/wrapper.pid'
+
+    def delete_pid(self):
+        f = os.remove(self.pidfile)
+
+    def write_pid(self):
+        with os.fdopen(os.open(self.pidfile, os.O_WRONLY | os.O_CREAT, 0600), 'w') as f:
+            f.write(str(self.pid))
+
+    def quit(self, signum, frame):
+        print '{}: killed by signal {}'.format(self.name, signum)
+        if self.p:
+            self.p.terminate()
+        self.delete_pid()
+        os._exit(0)
+
+    def run(self):
+        os.chdir(self.home)
+
+        imapfilter_cmd = ['imapfilter']
+
+        if self.verbose:
+            imapfilter_cmd.append('-v')
+
+        if self.debug:
+            log_debug = self.home + '/debug.log'
+            print 'Debug Log = {}'.format(log_debug)
+            imapfilter_cmd.append('-d')
+            imapfilter_cmd.append(log_debug)
+
+        if self.daemonize:
+            if os.fork() != 0:
+                os._exit(0)
+
+            os.setsid()
+
+            fd = os.open('/dev/null', os.O_RDONLY)
+            os.dup2(fd, 0)
+
+            log = os.open(self.home + '/imapfilter.log', os.O_WRONLY | os.O_CREAT, 0600)
+            os.dup2(log, 1)
+            os.dup2(log, 2)
+
+
+        self.pid = os.getpid()
+        signal.signal(signal.SIGHUP, self.quit)
+        signal.signal(signal.SIGINT, self.quit)
+        signal.signal(signal.SIGTERM, self.quit)
+        self.write_pid()
+
+        # Point to dbus for keyring access in the event this isn't started
+        # from a local GNOME session.  This expects another shell script to
+        # write this file from something like .bashrc
+        my_env = os.environ.copy()
+        with open(my_env['HOME'] + '/.dbus-session') as f:
+            my_env['DBUS_SESSION_BUS_ADDRESS'] = f.read().strip()
+
+        while True:
+            self.p = subprocess.Popen(imapfilter_cmd, env=my_env)
+            print("{}: started {} with pid {}".format(self.name, imapfilter_cmd, self.p.pid))
+            self.p.wait()
+            time.sleep(30)
+
+#
+# Main function, either run keyring or run the wrapper
+#
+if __name__ == '__main__':
+
+    if len(sys.argv) > 1:
+        action = sys.argv[1]
+    else:
+        action = 'imapfilter'
+
+
+    if action == 'keyring':
+
+        keyring_action = sys.argv[2]
+        server = sys.argv[3]
+        key = sys.argv[4]
+
+        keyring = Keyring('IMAP Filter ' + server, server, 'imap')
+
+        if keyring_action == 'set':
+            user = raw_input("Username: ")
+            secret = getpass.getpass()
+            keyring.set_credentials((user, secret))
+        elif keyring_action == 'get':
+            user, secret = keyring.get_credentials()
+            if key == 'password':
+                print secret
+            else:
+                print user
+
+    # action == 'imapfilter'
+    else:
+        #wrapper = ImapFilterWrapper(verbose=True, daemonize=False)
+        wrapper = ImapFilterWrapper(verbose=True, daemonize=True)
+        wrapper.run()
+
